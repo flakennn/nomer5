2d8428324e16b39442107a7b120d60ba723f765a
Дмитрий <@HOME-PC>
2022-04-29 00:39:50 +0300

    Добавьте файлы проекта.

---
+++ nomer5/nomer5.h
@@ -0,0 +1,13 @@
+#ifndef NOMER5_H_
+#define NOMER5_H_
+
+#include <iostream>
+
+
+using namespace std;
+
+bool InCheck(int num);
+void FillArr(int** arr, int n, int m);
+void OutArr(int** arr, int n, int m);
+
+#endif
